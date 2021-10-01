8066d857c2ee50e3509b9dabd89ad21783a95cf3
Devanshu24 <devshah.685@live.com>
2021-10-01 18:09:16 +0530

    Cleanup example

--- simple_regression.py
+++ simple_regression.py
@@ -9,19 +9,12 @@ if __name__ == "__main__":
     inputs = torch.linspace(-1, 1, 1000)
     labels = inputs / 2
     losses = []
-    weights = []
     for i in range(1000):
         out = n.forward({0: inputs[i], 1: 1})
         loss = F.mse_loss(out[0].reshape(1), labels[i].reshape(1))
         losses.append(loss.item())
-        cur_weights = n.weights[(0, 1)].item()
-        weights.append(cur_weights)
         n.backward({0: inputs[i], 1: 1}, 0.1, loss, lambda x: 2 * (x - labels[i]))
         n.reset()
-        assert torch.allclose(
-            n.weights[(0, 1)],
-            cur_weights - 0.1 * (2 * (out[0] - labels[i]) * inputs[i]),
-        )
     print(n.weights)
     plt.plot(losses)
     plt.show()
